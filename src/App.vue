50103e4a9d928ba00ac902451250b5c0713b121b
«KarahDotjs » <bintoukaramoko3@gmail.com>
2025-04-29 07:30:31 +0200

    Create new component SearchBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import MyBookShelf from './components/MyBookShelf.vue'
+
+
 
 </script>
 
 <template>
   <header>
-
+    <MyBookShelf />
   </header>
 
   <main>
---
+++ src/components/MyBookShelf.vue
@@ -0,0 +1,17 @@
+<script setup>
+import SearchBar from './components/SearchBar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 p-8">
+    <h1 class="text-3xl font-bold  flex justify-center mt-10">
+      My BookShelf 📚
+    </h1>
+    <input type="text">
+
+  </div>
+
+
+</template>
+
+<style scoped></style>
