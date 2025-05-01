90872f618419722a861019b2b2ef9abe749ad8aa
«KarahDotjs » <bintoukaramoko3@gmail.com>
2025-05-01 16:57:02 +0200

    fix: center last row of monthly books grid for better layout

--- src/components/MyBookShelf.vue
+++ src/components/MyBookShelf.vue
@@ -1,7 +1,6 @@
 <script setup>
 import SearchBar from "./SearchBar.vue";
 import BookCard from "./BookCard.vue";
-import BookCardLibrary from './BookCardLibrary.vue'
 
 
 import { ref } from "vue";
@@ -13,6 +12,29 @@ const handleSearch = (userInput) => {
   fetchBooks();
 };
 const books = ref([]);
+
+const booksOfTheMonth = ref([]);
+
+const fetchBooksOfTheMonth = async () => {
+  try {
+    const response = await fetch('https://openlibrary.org/search.json?q=bestseller');
+    const data = await response.json();
+    // On récupère seulement ceux qui ont une couverture
+    booksOfTheMonth.value = data.docs
+      .filter(book => book.cover_i)
+      .slice(0, 5); // max 5 livres
+  } catch (error) {
+    console.error("Erreur lors du chargement des livres du mois :", error);
+  }
+};
+
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  fetchBooksOfTheMonth();
+});
+
+
 
 // const myLibrary = ref([]);
 
@@ -28,17 +50,34 @@ const addTolibray = (book) => { };
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-100 p-8">
-    <h1 class="text-3xl font-bold flex justify-center mt-10">
-      My BookShelf 📚
-    </h1>
-    <SearchBar @rechercher="handleSearch" />
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8">
-      <BookCard v-for="book in books.filter(book => book.cover_i)" :key="book.key" :title="book.title"
-        :author="book.author_name" :coverId="book.cover_i" />
-    </div>
+  <div class="min-h-screen bg-gray-100 py-8">
+    <div class="max-w-screen-lg mx-auto px-4">
 
+      <h1 class="text-3xl font-bold text-center mt-10">
+        My BookShelf 📚
+      </h1>
+
+      <SearchBar @rechercher="handleSearch" />
+
+      <!-- Résultats de recherche -->
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8">
+        <BookCard v-for="book in books" :key="book.key" :title="book.title" :author="book.author_name"
+          :coverId="book.cover_i" />
+      </div>
+
+      <!-- Livres du mois -->
+      <div class="mt-12 bg-white rounded-xl shadow px-6 py-10">
+        <h2 class="text-2xl font-bold mb-4 text-center">📚 Livre(s) du mois</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          <BookCard v-for="book in booksOfTheMonth" :key="book.key" :title="book.title" :author="book.author_name"
+            :coverId="book.cover_i" />
+        </div>
+      </div>
+
+
+    </div>
   </div>
 </template>
+
 
 <style scoped></style>
