0177fd93dc59f327527f606de831798c32d37179
«KarahDotjs » <bintoukaramoko3@gmail.com>
2025-05-01 08:32:27 +0200

    fix: reverted BookCard to stable version after breaking changes

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -3,20 +3,7 @@ defineProps({
   title: String,
   author: Array,
   coverId: Number
-})
-
-const emit = defineEmits(['addToLibrary'])
-
-
-const addThisBook = () => {
-  emit('addToLibrary', {
-    title,
-    author,
-    coverId
-  })
-}
-
-
+});
 </script>
 
 <template>
@@ -27,14 +14,7 @@ const addThisBook = () => {
     </p>
     <img v-if="coverId" :src="`https://covers.openlibrary.org/b/id/${coverId}-M.jpg`" alt="Couverture"
       class="mx-auto mt-4 h-48 object-cover" />
-    <button @click="addThisBook"
-      class="mt-4 bg-blue-400 text-white py-1 px-3 rounded hover:bg-green-600 text-sm font-bold">
-      <font-awesome-icon icon="plus"" />
-      Ajouter à ma bibliothèque
-    </button>
-
   </div>
-
 </template>
 
 <style scoped></style>
--- src/components/MyBookShelf.vue
+++ src/components/MyBookShelf.vue
@@ -1,52 +1,42 @@
 <script setup>
-import SearchBar from './SearchBar.vue'
-import BookCard from './BookCard.vue'
+import SearchBar from "./SearchBar.vue";
+import BookCard from "./BookCard.vue";
 
-import { ref } from 'vue'
+import { ref } from "vue";
 
-
-const searchQuery = ref('')
+const searchQuery = ref("");
 
 const handleSearch = (userInput) => {
-  searchQuery.value = userInput
-  fetchBooks()
+  searchQuery.value = userInput;
+  fetchBooks();
+};
+const books = ref([]);
 
-}
-const books = ref([])
-
-const myLibrary = ref([])
+// const myLibrary = ref([]);
 
 async function fetchBooks() {
-  const response = await fetch(`https://openlibrary.org/search.json?q=${searchQuery.value}`)
+  const response = await fetch(
+    `https://openlibrary.org/search.json?q=${searchQuery.value}`
+  );
   const data = await response.json();
-  console.log(data);
-  books.value = data.docs
+  books.value = data.docs;
 }
 
-const addToLibrary = (book) => {
-  myLibrary.value.push({
-    ...book,
-    comment: '',
-    rating: null
-  })
-}
-
-
+const addTolibray = (book) => { };
 </script>
 
 <template>
   <div class="min-h-screen bg-gray-100 p-8">
-    <h1 class="text-3xl font-bold  flex justify-center mt-10">
+    <h1 class="text-3xl font-bold flex justify-center mt-10">
       My BookShelf 📚
     </h1>
     <SearchBar @rechercher="handleSearch" />
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8">
-      <BookCard v-for="book in books" :key="book.key" :title="book.title" :author="book.author_name" :coverId="book.cover_i
-        " @add="addToLibrary" />
+      <BookCard v-for="book in books.filter(book => book.cover_i)" :key="book.key" :title="book.title"
+        :author="book.author_name" :coverId="book.cover_i" />
+
     </div>
   </div>
-
-
 </template>
 
 <style scoped></style>
